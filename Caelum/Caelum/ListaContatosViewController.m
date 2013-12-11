b448b905c7f6737f6b9b7b0e25b293e910201c4c
ios3918 <ios3918@caelum57-09.local>
2013-12-11 10:28:33 -0200

    Source with TableView to show contact by list (array). Using just 1 section and showing Name's contact. And also add to reload data anytime when view will appear.

--- Caelum/Caelum/ListaContatosViewController.m
+++ Caelum/Caelum/ListaContatosViewController.m
@@ -8,6 +8,7 @@
 
 #import "ListaContatosViewController.h"
 #import "FormularioContatoViewController.h"
+#import "Contato.h"
 
 @implementation ListaContatosViewController
 
@@ -38,6 +39,46 @@
     
     // cria uma pilha de navegação na barra de navegação
     [self.navigationController pushViewController:form animated:YES];
+}
+
+// TableView - Seções? Linhas? O que?
+
+// Seções - agrupamento de linhas (por exemplo nos contatos organiza e agrupa contatos por letra, A / B / C)
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+// Nº de linhas
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.contatos count]; // envia mensagem para a lista e confere quantos contatos existe na mesma
+}
+
+// o que será exibido na linha
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * pool = @"pool";
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:pool];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:pool];
+    }
+    
+    //Contato * contato = [self.contatos objectAtIndex:indexPath.row];
+    Contato * contato = self.contatos[indexPath.row];
+    cell.textLabel.text = contato.nome;
+
+    return cell;
+}
+
+// quando o view irá aparecer ele recarrega os dados da tabela
+- (void) viewWillAppear:(BOOL)animated
+{
+    [self.tableView reloadData];
 }
 
 @end
