e3c832935f3293f9fb322de9919a30ec32eceb7c
ios3918 <ios3918@caelum57-09.local>
2013-12-13 14:39:08 -0200

    Customized with image and setting image with 32x32 pixels

--- Caelum/Caelum/ContatosMapaViewController.h
+++ Caelum/Caelum/ContatosMapaViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-@interface ContatosMapaViewController : UIViewController
+@interface ContatosMapaViewController : UIViewController <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapa;
 @property (weak) NSMutableArray * contatos; // pode ser weak porque ela é dependente dos contatos
 
--- Caelum/Caelum/ContatosMapaViewController.m
+++ Caelum/Caelum/ContatosMapaViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ContatosMapaViewController.h"
+#import "Contato.h"
 
 @interface ContatosMapaViewController ()
 
@@ -51,6 +52,8 @@
     MKUserTrackingBarButtonItem * btn = [[MKUserTrackingBarButtonItem alloc] initWithMapView:self.mapa];
     
     self.navigationItem.leftBarButtonItem = btn;
+    
+   // self.mapa.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -69,6 +72,41 @@
 {
     [super viewWillDisappear:animated];
     [self.mapa removeAnnotations:self.contatos];
+}
+
+// aqui você customiza o alfinete e o balão do mapa
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    
+    if ([annotation isKindOfClass:[MKUserLocation class]]) {
+        return nil;
+    }
+    
+    static NSString * pool = @"pool";
+
+    MKPinAnnotationView * pin = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:pool];
+
+    if (!pin){
+    
+        pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pool];
+    }else{
+        pin.annotation = annotation;
+    }
+
+    pin.pinColor = MKPinAnnotationColorRed;
+
+    pin.canShowCallout = YES;
+
+    Contato * contato = (Contato *)annotation;
+    
+    if (contato.foto) {
+        UIImageView *imagemContato = [[UIImageView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 32.0f, 32.0f)];
+        
+        imagemContato.image = contato.foto;
+        pin.leftCalloutAccessoryView = imagemContato;
+    }
+    
+    return pin;
+
 }
 
 @end
